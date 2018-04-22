dcb71849d87cb9dcd536c9eb7e30541f571cdd84
rayjialin <ruijialin@gmail.com>
2018-04-22 13:03:52 -0700

    completed w2wkd assignment - gallery

---
+++ Gallery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gallery
+//
+//  Created by ruijia lin on 4/22/18.
+//  Copyright © 2018 ruijia lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
